ec7b7f7735cf7892e75230aa70495aea4b8a47b1
PetitSamuel <petits@tcd.ie>
2020-04-21 22:53:17 +0200

    part B parallelizing'

--- part_b/csu33014-annual-partB-code.c
+++ part_b/csu33014-annual-partB-code.c
@@ -60,7 +60,7 @@ struct Queue
 {
   int front, rear, size;
   unsigned capacity;
-  struct iPerson *array;
+  struct person *array;
 };
 
 struct Queue *createQueue(unsigned capacity)
@@ -70,7 +70,7 @@ struct Queue *createQueue(unsigned capacity)
   queue->size = 0;
   queue->front = 0;
   queue->rear = capacity - 1;
-  queue->array = (struct iPerson *)malloc(queue->capacity * sizeof(struct iPerson));
+  queue->array = (struct person *)malloc(queue->capacity * sizeof(struct person));
   return queue;
 }
 
@@ -84,7 +84,7 @@ int isEmpty(struct Queue *queue)
   return (queue->size == 0);
 }
 
-void enqueue(struct Queue *queue, struct iPerson item)
+void enqueue(struct Queue *queue, struct person item)
 {
   if (isFull(queue))
   {
@@ -100,97 +100,17 @@ void enqueue(struct Queue *queue, struct iPerson item)
 
 // Function to remove an item from queue.
 // It changes front and size
-struct iPerson dequeue(struct Queue *queue)
+struct person dequeue(struct Queue *queue)
 {
   if (isEmpty(queue))
   {
     printf("TRIED TO ADD TO A QUEUE AT FULL CAPACITY\n");
     exit(1);
   }
-  struct iPerson item = queue->array[queue->front];
+  struct person item = queue->array[queue->front];
   queue->front = (queue->front + 1) % queue->capacity;
   queue->size = queue->size - 1;
   return item;
-}
-
-/*
-void less_redundant_find_reachable_recursive(struct person *current, int steps_remaining,
-                                             bool *reachable)
-{
-  if (reachable[person_get_index(current)])
-    return;
-
-  // mark current root person as reachable
-  reachable[person_get_index(current)] = true;
-  // now deal with this person's acquaintances
-  if (steps_remaining > 0)
-  {
-    int num_known = person_get_num_known(current);
-    for (int i = 0; i < num_known; i++)
-    {
-      struct person *acquaintance = person_get_acquaintance(current, i);
-      if (steps_remaining > 1)
-      {
-        find_reachable_recursive(acquaintance, steps_remaining - 1, reachable);
-      }
-      else
-      {
-        reachable[person_get_index(acquaintance)] = true;
-      }
-    }
-  }
-}
-
-// computes the number of people within k degrees of the start person;
-// less repeated computation than the simple original version
-int less_redundant_number_within_k_degrees(struct person *start,
-                                           int total_people, int k)
-{
-  bool *reachable;
-  int count;
-
-  // maintain a boolean flag for each person indicating if they are visited
-  reachable = malloc(sizeof(bool) * total_people);
-  for (int i = 0; i < total_people; i++)
-  {
-    reachable[i] = false;
-  }
-
-  // now search for all people who are reachable with k steps
-  less_redundant_find_reachable_recursive(start, k, reachable);
-
-  // all visited people are marked reachable, so count them
-  count = 0;
-  for (int i = 0; i < total_people; i++)
-  {
-    if (reachable[i] == true)
-    {
-      count++;
-    }
-  }
-  return count;
-}
-*/
-struct iPerson {
-  struct person* person;
-  int k;
-};
-struct iPerson createiPerson(int k, struct person* person)
-{
-  struct iPerson *iPerson = (struct iPerson *)malloc(sizeof(struct iPerson));
-  iPerson->k = k;
-  iPerson->person = person;
-  return iPerson;
-}
-
-int getKFromiPerson(struct iPerson* person)
-{
-  return person->k;
-}
-
-struct person* getPersonFromiPerson(struct iPerson* person)
-{
-  return person->person;
 }
 
 // computes the number of people within k degrees of the start person;
@@ -205,30 +125,47 @@ int less_redundant_number_within_k_degrees(struct person *start,
   }
 
   struct Queue *queue = createQueue(total_people);
-  int curr = person_get_index(start);
+  int pi = person_get_index(start);
 
-  visited[curr] = true;
-  struct iPerson a = createiPerson(k, start);
-  enqueue(queue, a);
+  visited[pi] = true;
+  enqueue(queue, *start);
   int count = 1;
+  int currentDepth = 0,
+      elementsToDepthIncrease = 1,
+      nextElementsToDepthIncrease = 0;
+
   while (!isEmpty(queue))
   {
     struct person current = dequeue(queue);
     int num_known = person_get_num_known(&current);
+
     for (int i = 0; i < num_known; i++)
     {
       struct person *acquaintance = person_get_acquaintance(&current, i);
-      curr = person_get_index(acquaintance);
-      if (!visited[curr])
+      pi = person_get_index(acquaintance);
+
+      if (!visited[pi])
       {
-        visited[curr] = true;
+        visited[pi] = true;
         count++;
         enqueue(queue, *acquaintance);
       }
+      else
+      {
+        nextElementsToDepthIncrease--;
+      }
+    }
+
+    nextElementsToDepthIncrease += num_known;
+    if (--elementsToDepthIncrease == 0)
+    {
+      if (++currentDepth >= k)
+        return count;
+      elementsToDepthIncrease = nextElementsToDepthIncrease;
+      nextElementsToDepthIncrease = 0;
     }
   }
 
-  printf("%d", total_people);
   return count;
 }
 
@@ -237,5 +174,55 @@ int less_redundant_number_within_k_degrees(struct person *start,
 int parallel_number_within_k_degrees(struct person *start,
                                      int total_people, int k)
 {
-  return number_within_k_degrees(start, total_people, k);
+  bool *visited = malloc(sizeof(bool) * total_people);
+
+  #pragma omp parallel for private(i) schedule(static,1)
+  for (int i = 0; i < total_people; i++)
+  {
+    visited[i] = false;
+  }
+
+  struct Queue *queue = createQueue(total_people);
+
+  visited[person_get_index(start)] = true;
+  enqueue(queue, *start);
+  int count = 1;
+  int currentDepth = 0,
+      elementsToDepthIncrease = 1,
+      nextElementsToDepthIncrease = 0;
+
+  while (!isEmpty(queue))
+  {
+    struct person current = dequeue(queue);
+    int num_known = person_get_num_known(&current);
+
+  #pragma omp parallel for private(i) public(visited, count, nextElementsToDepthIncrease) schedule(static,1)
+    for (int i = 0; i < num_known; i++)
+    {
+      struct person *acquaintance = person_get_acquaintance(&current, i);
+      int pi = person_get_index(acquaintance);
+
+      if (!visited[pi])
+      {
+        visited[pi] = true;
+        count++;
+        enqueue(queue, *acquaintance);
+      }
+      else
+      {
+        nextElementsToDepthIncrease--;
+      }
+    }
+
+    nextElementsToDepthIncrease += num_known;
+    if (--elementsToDepthIncrease == 0)
+    {
+      if (++currentDepth >= k)
+        return count;
+      elementsToDepthIncrease = nextElementsToDepthIncrease;
+      nextElementsToDepthIncrease = 0;
+    }
+  }
+
+  return count;
 }
